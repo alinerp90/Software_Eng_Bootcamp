95ab2de985b88ea52a1c3524a4dcf98b024e7459
Aline Rodrigues <aline_r_pereira@hotmail.com>
2024-03-07 11:11:56 +0000

    created new function

--- task1_variables/hello_world.py
+++ task1_variables/hello_world.py
@@ -16,3 +16,23 @@ print(user_name)
 user_age = input ("How old are you? ")
 print(user_age)
 print("Hello World")
+
+# improvements:
+
+def greeting(name, age):
+    name = input("What is your name? ")
+    age = input ("How old are you? ")
+
+"""  
+Pick any one of your GitHub repos.
+● Create 2 issues for things you think could be improved. Ideas for
+improvements include making new methods, adding constants, renaming
+variables and functions, or adding comments.
+● For each issue:
+○ Create a branch with a meaningful name.
+○ Implement the changes required by the issue.
+○ Commit and push your work.
+○ Create a PR for your changes.
+○ Merge in the PR and close the issue.
+● In a text file called repo.txt, paste the link to your repo. Add the file to this task’s folder.
+"""
